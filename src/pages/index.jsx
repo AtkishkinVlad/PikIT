a822e5e9ba113500dbb7c41a3020cebb8af34912
atkvs <vladatkishkin@vk.com>
2023-10-22 19:18:14 +0500

    bingo: update for talk

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import { useForm } from '@formcarry/react';
+import Image from "next/image";
 
 import { RedirectPage } from "@/components/RedirectPage/RedirectPage";
 
@@ -36,110 +37,94 @@ export default function Home() {
       <div className={styles.postcard__container}>
         <main className={styles.postcard__description}>
           <h1 className={styles.description__title}>
-            Менеджерское бинго Контура
+            Бинго курьезов на рабочих созвонах
           </h1>
           <p style={{ textAlign: 'start', marginBottom: '32px' }}>
-            Отмечай, что с тобой уже случалось на работе — посмотрим, насколько схожие у нас бывают ситуации. 
+            Отмечай, что с тобой уже случалось на созвоне — посмотрим, насколько схожие у нас бывают ситуации. 
           </p>
           <form onSubmit={submit} className={styles.grid_wrapper}>
-            <label htmlFor="1" className={styles.wide}>
-              <input id="1" name="1" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Увольнял сотрудника" />
+            <label htmlFor="1" className={styles.tall}>
+              <input id="1" name="1" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Получать приглашение на встречу с темой: «Надо поговорить»" />
               <h3 className={styles.description}>
-                Увольнял сотрудника
+                Получать приглашение на встречу с темой: «Надо поговорить»
               </h3>
             </label>
             <label htmlFor="2" className={styles.tall}>
-              <input id="2" name="2" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Вырастил тимлида" />
+              <input id="2" name="2" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Любоваться на кота, который залез в кадр" />
               <h3 className={styles.description}>
-                Вырастил тимлида
+                Любоваться на кота, который залез в кадр
               </h3>
             </label>
             <label htmlFor="3" className={styles.tall}>
-              <input id="3" name="3" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Заходил в команду, где все плохо и сделал хорошо" />
+              <input id="3" name="3" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Все друг друга перебивали" />
               <h3 className={styles.description}>
-                Заходил в команду, где все плохо и сделал хорошо
+                Видеть как все перебивают друг друга
               </h3>
             </label>
             <label htmlFor="4" className={styles.tall}>
-              <input id="4" name="4" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Вырастил команду в 2 раза" />
+              <input id="4" name="4" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Смотреть встречи в записи, потому что в календаре нет свободных окон" />
               <h3 className={styles.description}>
-                Вырастил команду в 2 раза
+                Смотреть встречи в записи, потому что в календаре нет свободных окон
               </h3>
             </label>
             <label htmlFor="5" className={styles.tall}>
-              <input id="5" name="5" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Настраивал джиру / ютрек" />
+              <input id="5" name="5" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Включать шумоподавление, потому что соседи начали ремонт" />
               <h3 className={styles.description}>
-                Настраивал джиру/ютрек
+                Включать шумоподавление, потому что соседи начали ремонт
               </h3>
             </label>
             <label htmlFor="6" className={styles.tall}>
-              <input id="6" name="6" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Работал больше чем в одной команде" />
+              <input id="6" name="6" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Выбирать забавный фон с ковром или пальмами" />
               <h3 className={styles.description}>
-                Работал больше чем в одной команде 
+                Выбирать забавный фон с ковром или пальмами
               </h3>
             </label>
             <label htmlFor="7" className={styles.tall}>
-              <input id="7" name="7" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Пережил выгорание" />
+              <input id="7" name="7" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Подключаться к внеплановому созвону с телефона" />
               <h3 className={styles.description}>
-                Пережил выгорание
+                Подключаться к внеплановому созвону с телефона
               </h3>
             </label>
-            <label htmlFor="8"  className={styles.big}>
-              <input id="8" name="8" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Признавался руководителю, что не вывозишь" />
+            <label htmlFor="8"  className={styles.tall}>
+              <input id="8" name="8" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Выключать камеру, когда не успеваешь пообедать" />
               <h3 className={styles.description}>
-                Признавался руководителю, что не вывозишь
+                Выключать камеру, когда не успеваешь пообедать
               </h3>
             </label>
             <label htmlFor="9"  className={styles.tall}>
-              <input id="9" name="9" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Выбивал бюджет на команду" />
+              <input id="9" name="9" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Отлучиться на минутку, потому что встречаешь курьера" />
               <h3 className={styles.description}>
-                Выбивал бюджет на команду
+                Отлучиться на минутку, потому что встречаешь курьера
               </h3>
             </label>
             <label htmlFor="10"  className={styles.tall}>
-              <input id="10" name="10" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Продавал заказчику, что фичу делать не надо" />
+              <input id="10" name="10" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Слушать докладчика когда встреча давно закончилась" />
               <h3 className={styles.description}>
-                Продавал заказчику, что фичу делать не надо
+                Слушать докладчика когда встреча давно закончилась
               </h3>
             </label>
-            <label htmlFor="11"  className={styles.wide}>
-              <input id="11" name="11" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Погружазился в сложную предметку" />
+            <label htmlFor="11"  className={styles.tall}>
+              <input id="11" name="11" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Путать часовые пояса и подключаться не в то время" />
               <h3 className={styles.description}>
-                Погружался в сложную предметку
+                Путать часовые пояса <br /> и подключаться не в то время
               </h3>
             </label>
             <label htmlFor="12"  className={styles.tall}>
-              <input id="12" name="12" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Внедрял скрам/канбан" />
+              <input id="12" name="12" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Путать часовые пояса и подключаться не в то время" />
               <h3 className={styles.description}>
-                Внедрял скрам/канбан
+                Ловить глюки интернета в самый важный момент
               </h3>
             </label>
-            <label htmlFor="13"  className={styles.tall}>
-              <input id="13" name="13" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Выходил из команды и она не разваливаллась" />
-              <h3 className={styles.description}>
-                Выходил из команды и она не разваливалась
-              </h3>
-            </label>
-            <label htmlFor="14"  className={styles.tall}>
-              <input id="14" name="14" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Уговорил бизнес выделить время на техдолг" />
-              <h3 className={styles.description}>
-                Уговорил бизнес выделить время на техдолг 
-              </h3>
-            </label>
-            <label htmlFor="15"  className={styles.tall}>
-              <input id="15" name="15" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Работал в команде из нескольких менеджеров" />
-              <h3 className={styles.description}>
-                Работал в команде из нескольких менеджеров  
-              </h3>
-            </label>
-            <label htmlFor="16"  className={styles.wide}>
-              <input id="16" name="16" onClick={onClickHandler} type="checkbox" className={styles.checkbox} value="Существенно ускорил ТТМ" />
-              <h3 className={styles.description}>
-                Существенно ускорил ТТМ   
-              </h3>
-            </label>
-            {checkCount > 0 && <button className={`${styles.button}`} type="submit">Поделиться своим бинго </button>}
           </form>
+          <footer style={{
+            marginTop: '2rem',
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            alignItems: 'center'
+          }}>
+            {checkCount > 0 && <button className={`${styles.button}`} type="submit">Поделиться своим бинго </button>}
+            <Image width={256} height={256} src="https://s.kontur.ru/common-v2/logos/logo-talk-32.svg" alt="Логотип Толка" />
+          </footer>
         </main>
       </div>
       <div className={styles.light1} />
